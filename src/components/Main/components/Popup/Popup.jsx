12d6c47cfb8f12bd530c219c3e9099e2ef84c4fe
Renato <renato.rfco@gmail.com>
2026-08-07 18:08:39 +0200

    Refactor classes Popup

--- src/components/Main/components/Popup/Popup.jsx
+++ src/components/Main/components/Popup/Popup.jsx
@@ -2,22 +2,17 @@ export default function Popup(props) {
     const {title, children} = props;
 
     return(
-        <section className="popup popupedit">
-        <div className="popup__container">
-          <button
-            type="button"
-            id="closeEditButton"
-            className="popup__close-button popup__close-headerButton"
-          >
-            <img
-              src="./images/Popup_Close-Icon.png"
-              className="popup__close-icon"
-              alt="Botão de fechar"
-            />
-          </button>
-          <h2 className="popup__container-title">{title}</h2>
-          {children}
-        </div>
-      </section>
+        <div className="popup">
+            <div className="popup__container">
+                <button
+                    type="button"
+                    id="closeButton"
+                    className="popup__close-button"
+                >
+                </button>
+                <h2 className="popup__container-title">{title}</h2>
+                {children}
+            </div>
+      </div>
     );
 }
