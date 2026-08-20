8a696a3a7c30f35e7a0a6c5d0402a3e5a50c040b
Renato <renato.rfco@gmail.com>
2026-08-20 14:19:35 +0200

    Create currentUser variable/method to get user info from api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,23 @@
+import { useState, useEffect } from 'react'
+import api from '../utils/api.jsx'
 import Header from './Header/Header.jsx'
 import Main from './Main/Main.jsx'
 import Footer from './Footer/Footer.jsx'
 
 function App() {
 
+  const [currentUser, setCurrentUser] = useState();
+
+  useEffect(()=>{
+    api.getUserInfo()
+      .then((data)=>{
+        setCurrentUser(data);
+      })
+      .catch((err)=>{
+        console.log(err);
+      });
+  },[]);
+  
   return (
     
     <div className="page">
